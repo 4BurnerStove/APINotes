e7d2594e25e9c66201ccc77bf0dbd3799c9f4e36
Andre Morryson <amorrysson77@gmail.com>
2023-07-07 10:59:49 -0300

    Create a token for user

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,5 +1,8 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
+
 const { compare } = require('bcryptjs')
 
 class SessionController {
@@ -12,12 +15,20 @@ class SessionController {
         throw new AppError('Email e/ou senha incorreta', 401)
      }
 
+
      const passwordMatched = await compare(password, user.password)
+
      if(!passwordMatched) {
         throw new AppError('Email e/ou senha incorreta', 401)
      }
+
+     const { secret, expiresIn} = authConfig.jwt
+     const token = sign({}, secret, {
+        subject: String(user.id),
+        expiresIn
+     })
      
-     return res.json(user)
+     return res.json({user, token})
     }
 }
 
