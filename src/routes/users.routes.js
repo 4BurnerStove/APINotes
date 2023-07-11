bf1c91892a0bac55cadce919e1e18af0385d797d
Andre Morryson <amorrysson77@gmail.com>
2023-07-11 12:44:21 -0300

    UserAvatarController

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,19 +1,21 @@
 const { Router } = require('express')
 const multer = require('multer')
 const uploadConfig = require('../configs/upload')
-const 
 
 const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
+
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 const userRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 userRoutes.post('/', usersController.create)
 userRoutes.put('/', ensureAuthenticated, usersController.update)
-userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) =>  )
+userRoutes.patch('/avatar', ensureAuthenticated, upload.single("avatar"),  userAvatarController.update)
 
 module.exports = userRoutes
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('express-async-errors')
 const migrationsRun = require('./database/sqlite/migrations')
 
 const AppError = require('./utils/AppError')
+const uploadConfig = require('./configs/upload')
 
 const express = require('express')
 
@@ -14,8 +15,9 @@ const app = express()
 migrationsRun()
 app.use(express.json())
 
-app.use(routes)
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
+app.use(routes)
 
 app.use((error, req, res, next) => {
   if (error instanceof AppError) {
